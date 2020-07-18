ae7e34ead69e37c3303c773a53b3563d2736fcff
Vladymyr <vladymyr.a13@gmail.com>
2020-07-19 00:12:23 +0300

    теперь меню закрывается при нажатии  на ссылку

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+"use strict";
+
 let menuButton = document.querySelector(".navbar__menu-button");
 let headerMenu = document.querySelector(".menu");
 menuButton.onclick = function () {
   headerMenu.classList.toggle("menu_active");
+  headerMenu.classList.remove("hidden__menu");
 };
+
+let a = document.querySelectorAll(".menu__link");
+console.log(a);
+
+[].forEach.call(a, function (el) {
+  el.onclick = function (e) {
+    console.log(e);
+    headerMenu.classList.add("hidden__menu");
+    headerMenu.classList.remove("menu_active");
+  };
+});
+
+// [1, 2, 3].forEach(function (num) {
+//   console.log(num);
+// });
